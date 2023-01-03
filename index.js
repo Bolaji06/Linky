1bb61b51c8e5a5310eb8d432c8c250b6010f1285
Bolaji06 <joshspencer0603@gmail.com>
2023-01-02 16:57:57 -1200

    first linky commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const menuEl = document.querySelector('.hambuger-menu');
+const secondaryNav = document.querySelector('.secondary-navigation');
+const primaryNav = document.querySelector('.primary-navigation');
+const closeBtn = document.querySelector('.close-btn')
+
