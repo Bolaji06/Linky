921a2a279e4eb3cb3621729acd7f6549b32a8ccf
Bolaji06 <joshspencer0603@gmail.com>
2023-01-18 11:17:59 -1200

    work on file type selection

--- qr-code.js
+++ qr-code.js
@@ -3,6 +3,10 @@ const qrCodeImgEl = document.querySelector('.qr-image');
 const qrCodeBtn = document.querySelector('.qr-btn');
 const marginOkBtn = document.querySelector('.margin-ok');
 const inputColor = document.querySelector('#color-picker');
+const fileTypeSelect = document.querySelector('#file-type');
+const downloadBtn = document.querySelector('.download-btn');
+
+
 const uri = `http://api.qrserver.com/v1/create-qr-code/?data=`
 
 
@@ -14,11 +18,6 @@ function createQRCode(){
     //let uriWithMargin = qrCodeUri.concat('&margin=40', '&color=f00');
     qrCodeImgEl.src = qrCodeUri;
 }
-
-// if (qrCodeInput === ' '){
-//     //qrCodeBtn.disable = true;
-//     qrCodeBtn.style.backgroundColor = 'red';
-// }
 
 qrCodeBtn.addEventListener('click', createQRCode);
 marginOkBtn.addEventListener('click', addingMarginParameter)
@@ -45,8 +44,41 @@ function getChange(event){
     let colorChange = `${uri}${qrCodeInput}&size=250x250&color=${removeHarsh}`;
     qrCodeImgEl.src += colorChange;
     console.log(getChangeColor); 
-   
 }
+
+fileTypeSelect.addEventListener('click', (e)=>{
+    //console.log(e.target.value);
+    const fileType = e.target.value;
+
+    if (fileType === 'png'){
+        console.log('png');
+        const downloadPng = ``
+    }
+    else if (fileType === 'jpeg'){
+        console.log('jpeg');
+    }
+    else if (fileType === 'gif'){
+        console.log('gif');
+    }
+    else{
+        console.log('svg');
+    }
+})
+downloadBtn.addEventListener('click', downloadQRCode);
+
+function downloadQRCode(){
+    //qrCodeImgEl.setAttribute('href', "https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100&format=png");
+}
+const dl = document.querySelector('.download-link');
+//dl.setAttribute('href', "https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100&format=svg")
+//dl.addEventListener('click', ()=>{
+    //dl.setAttribute('download', 'qr-code');
+    
+//})
+
+
+
+
 
 // let myName = 'BOLAJI';
 // let remove = myName.slice(1);
